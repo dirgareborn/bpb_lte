8d35c7766f574ca7a843ad929cc9b633db07e0da
Dirga <kikifatiroh@gmail.com>
2024-08-04 11:49:37 +0800

    edit image asset

--- resources/views/front/partials/header.blade.php
+++ resources/views/front/partials/header.blade.php
@@ -1,45 +1,20 @@
-  <!-- Header Start -->
-  <div class="container-fluid header bg-white p-0">
+ <div class="container-fluid header bg-white p-0">
             <div class="row g-0 align-items-center flex-column-reverse flex-md-row">
                 <div class="col-md-6 p-5 mt-lg-5">
-					 @forelse($homeSliderBanners as $banner)
-					 <h1 class="display-5 animated fadeIn mb-4">{{ $banner['title']}}</h1>
-                    <p class="animated fadeIn mb-4 pb-2">{{ $banner['alt'] }}.</p>
-					 @empty
-                    <h1 class="display-5 animated fadeIn mb-4">Ayo Menikah <span class="text-primary">Di Gedung UNM</span> Dengan Harga Bersahabat</h1>
-                    <p class="animated fadeIn mb-4 pb-2">Kami menyediakan Gedung Pernikahan dengan Kapasitas 2000 Undangan dengan fasilitas parkiran terluas se Kota Makassar.</p>
-					 @endforelse
-                    <a href="" class="btn btn-primary py-3 px-5 me-3 animated fadeIn">Booking</a>
+                    <h1 class="display-5 animated fadeIn mb-4">{{$page_title ?? ''}}</h1> 
+                        <nav aria-label="breadcrumb animated fadeIn">
+                        <ol class="breadcrumb text-uppercase">
+                        @for($i = 1; $i <= count(Request::segments()); $i++)
+                            <li class="breadcrumb-item"><a href="{{ URL::to( implode( '/', array_slice(Request::segments(), 0 ,$i, true)))}}">{{strtoupper(Request::segment($i))}}</a></li>
+                        @endfor
+                        </ol>
+                    </nav>
                 </div>
                 <div class="col-md-6 animated fadeIn">
-                    <div class="owl-carousel header-carousel">
-                        @forelse($homeSliderBanners as $banner)
-						
-						@if($banner['image'] > 2)
-                        <div class="owl-carousel-item">
-                            <img class="img-fluid" src="{{ asset('front/images/banners/'. $banner['image'])}}" alt="">
-							</div>
-						@else
-							<div class="owl-carousel-item">
-							<img class="img-fluid" src="{{ asset('front/images/banners/'. $banner['image'])}}" alt="">
-							</div>
-							<div class="owl-carousel-item">
-								<img class="img-fluid" src="{{url('front/img/carousel-1.jpg') }}" alt="">
-							</div>
-							<div class="owl-carousel-item">
-								<img class="img-fluid" src="{{url('front/img/carousel-2.jpg') }}" alt="">
-							</div>
-						@endif
-                        @empty
-                        <div class="owl-carousel-item">
-                            <img class="img-fluid" src="{{url('front/img/carousel-1.jpg') }}" alt="">
-                        </div>
-                        <div class="owl-carousel-item">
-                            <img class="img-fluid" src="{{url('front/img/carousel-2.jpg') }}" alt="">
-                        </div>
-                        @endforelse
-                    </div>
+                    <img class="img-fluid" src="{{ asset('front/img/header.jpg') }}" style="height:350px" alt="">
                 </div>
             </div>
         </div>
-        <!-- Header End -->
+
+ <div class="container-fluid bg-primary mb-5 wow fadeIn" data-wow-delay="0.1s" style="padding: 35px;">
+ </div>
